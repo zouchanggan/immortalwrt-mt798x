71c65986db4ca7e70045a9b949fae492e10a6152
padavanonly <padavanonly@github.com>
2024-04-15 10:20:47 +0800

    update wwan packages

--- target/linux/mediatek/image/mt7981.mk
+++ target/linux/mediatek/image/mt7981.mk
@@ -3,7 +3,7 @@ KERNEL_LOADADDR := 0x48080000
 MT7981_USB_PKGS := automount blkid blockdev fdisk \
     kmod-nls-cp437 kmod-nls-iso8859-1 kmod-usb2 kmod-usb3 \
     luci-app-usb-printer luci-i18n-usb-printer-zh-cn \
-    kmod-usb-net-rndis usbutils
+    kmod-usb-net-rndis usbutils luci-app-usbmodem
 
 define Device/mt7981-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
